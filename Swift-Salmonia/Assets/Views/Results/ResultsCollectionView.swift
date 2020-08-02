a1c4850378742952b1e48e6dbd68e641ec99cc6c
Asano Kazuna <nasawke.am@gmail.com>
2020-08-02 08:02:29 -0400

    Update responsive design on ResultsCollectionView.

--- Swift-Salmonia/Assets/Views/Results/ResultsCollectionView.swift
+++ Swift-Salmonia/Assets/Views/Results/ResultsCollectionView.swift
@@ -38,43 +38,42 @@ struct ResultStack: View {
     }
     
     var body: some View {
-        VStack(alignment: .trailing, spacing: 0) {
+        VStack(spacing: 0) {
             HStack {
-                Text(result.danger_rate.string + "%")
+                Text(result.danger_rate.string + "%").frame(height: 5.vw).padding(0)
                 Spacer()
                 Group {
                     ForEach(result.weapons, id: \.self) { weapon in
-                        URLImage(URL(string: weapon.weapon)!, content: {$0.image.resizable().frame(width: 30, height: 30)
+                        URLImage(URL(string: weapon.weapon)!, content: {$0.image.resizable().frame(width: 10.vw, height: 10.vw)
                         })
                     }
                 }
-            }
+            }.frame(height: 12.vw)
             HStack {
                 Group {
                     URLImage(URL(string: "https://app.splatoon2.nintendo.net/images/bundled/3aa6fb4ec1534196ede450667c1183dc.png")!, content: {$0.image.resizable()})
-                        .frame(width: 20, height: 20)
-                    Text(result.golden_eggs.string).frame(width: 30)
+                        .frame(width: 5.vw, height: 5.vw)
+                    Text(result.golden_eggs.string).frame(width: 10.vw)
                     Spacer()
                     URLImage(URL(string: "https://app.splatoon2.nintendo.net/images/bundled/78f61aacb1fbb50f345cdf3016aa309e.png")!, content: {$0.image.resizable()})
-                        .frame(width: 20, height: 20)
-                    Text(result.power_eggs.string).frame(width: 45)
+                        .frame(width: 5.vw, height: 5.vw)
+                    Text(result.power_eggs.string).frame(width: 12.vw)
                     Spacer()
                 }
                 Group {
                     URLImage(URL(string: "https://app.splatoon2.nintendo.net/images/bundled/c003ffe0a5580e4c8b1bc9df1e0a30d2.png")!, content: {$0.image.resizable()})
-                        .frame(width: 50, height: 20)
-                    Text("99").frame(width: 30)
+                        .frame(width: 11.5.vw, height: 5.vw)
+                    Text("99").frame(width: 8.vw)
                     Spacer()
                     URLImage(URL(string: "https://app.splatoon2.nintendo.net/images/bundled/5d447dcfcb3b0c31ffb2efca58a6e799.png")!, content: {$0.image.resizable()})
-                        .frame(width: 50, height: 20)
-                    Text("99").frame(width: 30)
+                        .frame(width: 11.5.vw, height: 5.vw)
+                    Text("99").frame(width: 8.vw)
                 }
             }
         }
-        .padding(10)
-        .frame(maxWidth: .infinity, alignment: .topLeading)
-        .font(.custom("Splatfont2", size: 18))
-        //        .background(Color.orange)
+        .padding(2.vw)
+        .frame(height: 20.vw)
+        .font(.custom("Splatfont2", size: 5.vw))
     }
 }
 
