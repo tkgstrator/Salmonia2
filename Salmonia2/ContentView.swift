d40a90d79772212cdb1a528b22523b9e8275f240
Asano Kazuna <nasawke.am@gmail.com>
2020-09-27 06:10:08 -0400

    Initial Commit

---
+++ Salmonia2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Salmonia2
+//
+//  Created by devonly on 2020-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Salmonia2Tests/Salmonia2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Salmonia2Tests.swift
+//  Salmonia2Tests
+//
+//  Created by devonly on 2020-09-27.
+//
+
+import XCTest
+@testable import Salmonia2
+
+class Salmonia2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
